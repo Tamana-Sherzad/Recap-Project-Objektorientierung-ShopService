515cfd0a91afa5d5f7abb5d9fbb65c16419196fc
sherzad <t.sherzad@web.de>
2023-12-19 11:54:27 +0100

    GetProductById Return Optional

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -12,21 +11,20 @@ public class ShopService {
     public Order addOrder(List<String> productIds) {
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
-            //  Product productToOrder = productRepo.getProductById(productId);
-            //if (productToOrder == null) {
-            Optional<Product> productToOrder = productRepo.getProductById(productId);
-            if (productToOrder.isEmpty()) {
+            Product productToOrder = productRepo.getProductById(productId);
+            if (productToOrder == null) {
                 System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
                 return null;
             }
-            products.add(productToOrder.get());
+            products.add(productToOrder);
         }
 
         Order newOrder = new Order(UUID.randomUUID().toString(), products, Order.OrderStatus.COMPLETED);
 
         return orderRepo.addOrder(newOrder);
     }
-        public List<Order> returnOrderListByStatus (Order.OrderStatus orderStatus){
-            return orderRepo.getOrders().stream().filter(o -> o.orderStatus().equals(orderStatus)).collect(Collectors.toList());
-        }
+
+    public List<Order> returnOrderListByStatus(Order.OrderStatus orderStatus){
+        return orderRepo.getOrders().stream().filter(o-> o.orderStatus().equals(orderStatus)).collect(Collectors.toList());
     }
+}
